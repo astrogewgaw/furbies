a6acba7806f1979b86a83c5e06c5a3a6aed7e9c2
astrogewgaw <ujjwalpanda97@gmail.com>
2021-07-17 11:26:45 +0530

    :art: Minor edits to scraper.

--- scrap.py
+++ scrap.py
@@ -1,11 +1,19 @@
 if __name__ == "__main__":
 
+    import json
     import pandas as pd  # type: ignore
     from textwrap import dedent
 
-    df = pd.read_csv(
-        dedent(
-            """
+    with open("furbies.json", "w+") as f:
+        json.dump(
+            fp=f,
+            obj={
+                "data": {
+                    str(int(key) + 1): value
+                    for key, value in json.loads(
+                        pd.read_csv(
+                            dedent(
+                                """
             https://
             docs.google.com/
             spreadsheets/
@@ -14,11 +22,12 @@ if __name__ == "__main__":
             export?format=csv&
             gid=1560822367
             """
+                            )
+                            .replace("\n", "")
+                            .strip()
+                        ).to_json(orient="index")
+                    ).items()
+                }
+            },
+            indent=4,
         )
-        .replace("\n", "")
-        .strip()
-    ).to_json(
-        "furbies.json",
-        indent=4,
-        orient="index",
-    )
